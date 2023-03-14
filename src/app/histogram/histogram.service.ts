bedde9ebffbed722c4ae4f8bf4aca340950c20c1
Stéphane Bouget <stephane.bouget@orange.com>
2023-03-14 14:11:08 +0100

    wip working <= -1

--- src/app/histogram/histogram.service.ts
+++ src/app/histogram/histogram.service.ts
@@ -236,12 +236,12 @@ export class HistogramService {
       barW = barW / visibleChartsCount;
       x = shift + (ratioX / visibleChartsCount) * barX;
     } else if (d.partition[1] <= -1) {
-      shift = padding;
+      shift = padding + logView.p0P * chartW.p0P;
       barW =
         Math.log10(Math.abs(d.partition[0])) -
         Math.log10(Math.abs(d.partition[1]));
       barW = barW / visibleChartsCount;
-      x = shift + (ratioX / visibleChartsCount) * barX - barW * ratioX;
+      x = shift - (ratioX / visibleChartsCount) * barX;
     } else if (d.partition[1] < 0) {
       // shift = chartW * (1 + n) + padding;
       // x = shift - ratioX * barX * (1 + n);
